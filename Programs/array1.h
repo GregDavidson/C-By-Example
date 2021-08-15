225e348f5bced49add4842f02eb0f3ef49879a32
GregDavidson <greg.davidson+git@gmail.com>
2021-08-14 19:33:11 -0700

    This got left behind from the old repository!

---
+++ Programs/array1.h
@@ -0,0 +1,7 @@
+#define ARRAY_SIZE(array) (sizeof (array) / sizeof (array)[0])
+
+#define ARRAY_END(array) (array + ARRAY_SIZE(array))
+
+#define ARRAY_LOOP(pointer, array) \
+	for ( (pointer) = (array) ; (pointer) < ARRAY_END(array) ; (pointer)++ )
+
